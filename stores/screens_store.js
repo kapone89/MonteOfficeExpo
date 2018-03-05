8f37b38160f3943d7c7bf51f17577c6b9a5a623d
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2018-03-05 12:33:33 +0100

    add new predefined screens

--- stores/screens_store.js
+++ stores/screens_store.js
@@ -5,10 +5,15 @@ import { stringify } from 'query-string';
 import QueryGiphyApi from "../actions/screens/query_giphy_api";
 
 const predefined = [
-  new Screen({id: 1, name: "wifi credentials", website: "http://freakone.pl/monte/wifi.html" }),
-  new Screen({id: 2, name: "instafeed", website: "http://freakone.pl/monte/4.html" }),
-  new Screen({id: 3, name: "Classic Programmers Paintings", website: "http://cpp.kapone89.ml" }),
-  new Screen({id: 4, name: "monte logo", website: "http://jsbin.com/wokovo", thumb: "http://i.imgur.com/P9zGgt0.png" }),
+  new Screen({id: 1, name: "cdos.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/cdos.png" }),
+  new Screen({id: 2, name: "cdos_2.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/cdos_2.png" }),
+  new Screen({id: 3, name: "monte.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/monte.png" }),
+  new Screen({id: 4, name: "monte_dark.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/monte_dark.png" }),
+  new Screen({id: 5, name: "monte_red.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/monte_red.png" }),
+  new Screen({id: 6, name: "monte_red_2.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/monte_red_2.png" }),
+  new Screen({id: 7, name: "wifi.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/wifi.png" }),
+  new Screen({id: 8, name: "wifi_dark.png", website: "http://max.kapone89.ml/#https://raw.githubusercontent.com/kapone89/MonteOfficeExpo/static/screen/wifi_dark.png" }),
+  new Screen({id: 9, name: "Classic Programmers Paintings", website: "http://cpp.kapone89.ml" }),
 ]
 
 class ScreensStore {
